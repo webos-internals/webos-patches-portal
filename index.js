83d2325feb5cb7a2600296b9b5041ad855d715f1
Patrick Roberts <Patrick@silentbluesystems.com>
2011-01-11 10:58:23 -0800

    gitWrapper class now can pull tags and branches from a git repo

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var express=require('express');
+//var contrib = require('express-contrib');
+//var helpers = require('express-helpers');
+var wrapper= require('./lib/gitWrapper');
+var gitWrapper=new wrapper.client({"path":"/Users/halfhalo/src/modifications"},function(err){
+	if(err)
+	{
+		console.log(err)
+	}
+	else
+	{
+		//console.log("Success!")
+
+	}
+});
+gitWrapper.getBranches(function(branches,err){
+	console.log(branches)
+})
