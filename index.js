9b525e0e6301d26acad34fe363027b01ce452608
Patrick Roberts <Patrick@silentbluesystems.com>
2011-01-14 12:10:58 -0800

    Cut out the regex stuff and stick it in an external module, and start setting up the template system with a custom ejs

--- index.js
+++ index.js
@@ -1,14 +1,30 @@
 var express=require('express');
+var Log = require('coloured-log')
+,
+log = new Log(Log.DEBUG)
+var ejs=require('ejs')
+var fs=require('fs')
 var patches=require("./lib/patch");
 var patch=new patches.patch()
 var app = express.createServer();
+app.use(express.cookieDecoder());
+app.use(express.session());
 app.get('/', function(req, res){
-    res.send('Hello World');
+	//console.log(req)
+	log.info("Request From: "+req.headers.host+" -- "+req.method+" "+req.url+"\n UA: "+req.headers['user-agent'])
+    res.send(render('index'));
 });
 
-//app.listen(3000);
+app.listen(3000);
 //var contrib = require('express-contrib');
 //var helpers = require('express-helpers');
+function render(path)
+{
+	var str="";
+	var layout=fs.readFileSync("./templates/application.layout.html","utf8");
+	str+=layout;
+	return str;
+}
 var wrapper= require('./lib/gitWrapper');
 var gitWrapper=new wrapper.client({"path":"/Users/halfhalo/src/modifications"},function(err){
 	if(err)
@@ -19,6 +35,7 @@ var gitWrapper=new wrapper.client({"path":"/Users/halfhalo/src/modifications"},f
 	{
 	}
 });
+patch.find()
 gitWrapper.getBranches(function(branches,err){
 	//console.log(branches)
 },true)
