524b5df3f3576fae4178e5c9ae614f51fda6bf3a
Patrick Roberts <patrick@silentbluesystems.com>
2011-01-31 14:38:51 -0800

    template testing

--- server.js
+++ server.js
@@ -5,14 +5,16 @@ var jade=require('jade')
 var fs=require('fs')
 var patches=require("./lib/patch");
 var patch=new patches.patch();
+var tmplt=require('./lib/template');
+
 var app = express.createServer();
 //app.use(express.cookieDecoder());
 //app.use(express.session());
-app.set('views', __dirname + '/views');
-app.set('view engine', 'jade');
 app.use(express.staticProvider(__dirname + '/public'));
 app.get("/",function(req,res){
-		res.send("Index")
+		var template= new tmplt.t();
+		template.header({"title":"Test App","url":"#"},[{"name":"Test","url":"#","class":"first active"}],[])
+		res.send(template.build());
 })
 try{
 	log.info("Starting Express Server on port 3000")
