d206b6b397ad923ed60bf6cac87d223d49dc6d9f
Patrick Roberts <Patrick@silentbluesystems.com>
2011-04-11 15:46:46 -0700

    Cluster + Express now setup

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-require("./server");
+var cluster=require('cluster')
+, express = require('express')
+, http = require('http');
+var app = express.createServer();
+
+app.get('/', function(req, res){
+  res.send('Hello World');
+	console.log("Hi")
+});
+
+cluster(app)
+  .set('workers', 4)
+	.use(cluster.pidfiles())
+  .use(cluster.debug())
+	.use(cluster.reload())
+	.use(cluster.stats({ connections: true, requests: true }))
+  .use(cluster.logger())
+	.use(cluster.cli())
+  .listen(3000);
