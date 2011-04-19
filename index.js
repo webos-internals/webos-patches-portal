a6ed5803a2a09d87a90f2e64af39a88a5fa1633c
Patrick Roberts <Patrick@silentbluesystems.com>
2011-04-19 16:56:44 -0700

    Mysql Stuff

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
-var cluster=require('cluster')
-, express = require('express')
-, http = require('http');
+var cluster=require('cluster'), express = require('express'), http = require('http');
+var database=require('./lib/Database');database=new database.database({});
 var app = express.createServer();
-
+database.load(function(){
+	
+})
 app.get('/', function(req, res){
   res.send('Hello World');
 });
 
 cluster(app)
-  .set('workers', 4)
+  .set('workers', 1)
 	.use(cluster.pidfiles())
   .use(cluster.debug())
-	.use(cluster.reload())
-	.use(cluster.stats({ connections: true, requests: true }))
+	//.use(cluster.reload())
+	//.use(cluster.stats({ connections: true, requests: true }))
   .use(cluster.logger())
-	.use(cluster.cli())
+	//.use(cluster.cli())
   .listen(3000);
