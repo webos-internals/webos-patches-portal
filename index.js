09099ec66365cef812efcdbd2a710f7441810c3d
Patrick Roberts <Patrick@silentbluesystems.com>
2011-04-20 16:08:28 -0700

    cloudfoundry support, well, the start of it

--- index.js
+++ index.js
@@ -1,19 +1,27 @@
+require.paths.unshift('./node_modules')
 var cluster=require('cluster'), express = require('express'), http = require('http');
+var render=require('./lib/Render');render=new render.r();
+
 var database=require('./lib/Database');database=new database.database({});
 var app = express.createServer();
 database.load(function(){
 	
 })
+var users=require("./lib/Users");users=new users.users({});
 app.get('/', function(req, res){
-  res.send('Hello World');
+	users.getLogin(req,res,function(obj){
+		render.renderPage(obj.page,obj.options,function(file){
+			res.send(file);
+		},obj.layout)
+	})
 });
 
-cluster(app)
-  .set('workers', 1)
-	.use(cluster.pidfiles())
-  .use(cluster.debug())
+//cluster(app)
+//  .set('workers', 1)
+//	.use(cluster.pidfiles())
+//  .use(cluster.debug())
 	//.use(cluster.reload())
 	//.use(cluster.stats({ connections: true, requests: true }))
-  .use(cluster.logger())
+//  .use(cluster.logger())
 	//.use(cluster.cli())
-  .listen(3000);
+  app.listen(process.env.VMC_APP_PORT||3000);
